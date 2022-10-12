04b002f4963f405238f79a8013558fa103070e57
daniel jackson <deejack@daniels-MacBook-Pro.local>
2022-10-11 21:59:39 -0400

    working version of tip calculator

---
+++ tip_calculator.py
@@ -0,0 +1,21 @@
+def get_user_inputs(): 
+  cost_of_food = int(input('Enter the cost of your food : '))
+  number_of_payers = int(input('How many people will split this bill : '))
+  tip_percentage = int(input('What is the percentage of tip : '))
+  return (cost_of_food, number_of_payers, tip_percentage)
+
+def compute_total_bill(cost_of_food, number_of_payers, tip_percentage, sales_tax):
+  tip_amount = cost_of_food * (tip_percentage/100)
+  tax = cost_of_food * sales_tax
+  total_bill = (tip_amount + cost_of_food + tax)
+  print('Total bill: $',total_bill)
+  if number_of_payers > 0: 
+     splited_bill = total_bill / number_of_payers
+     print ('Each person should pay $',splited_bill)
+
+def main():
+  SALES_TAX = 0.1
+  cost_of_food, number_of_payers, tip_percentage = get_user_inputs()
+  compute_total_bill(cost_of_food, number_of_payers, tip_percentage, SALES_TAX)
+
+main()
