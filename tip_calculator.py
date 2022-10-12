493e61f643d2e61c7eefbd96687f9ef77451baf8
daniel jackson <deejack@daniels-MacBook-Pro.local>
2022-10-11 22:12:47 -0400

    format total and splitted bill

--- tip_calculator.py
+++ tip_calculator.py
@@ -8,10 +8,10 @@ def compute_total_bill(cost_of_food, number_of_payers, tip_percentage, sales_tax
   tip_amount = cost_of_food * (tip_percentage/100)
   tax = cost_of_food * sales_tax
   total_bill = (tip_amount + cost_of_food + tax)
-  print('Total bill: $',total_bill)
+  print('Total bill: ${:0.2f}'.format(total_bill))
   if number_of_payers > 0: 
      splited_bill = total_bill / number_of_payers
-     print ('Each person should pay $',splited_bill)
+     print ('Each person should pay ${:0.2f}'.format(splited_bill))
 
 def main():
   SALES_TAX = 0.1
